2fe63d6a62e942fe9cd45a0260e53d4f63608877
Casttle <cberry1222@gmail.com>
2018-11-20 19:19:25 -0600

    Updates

---
+++ precache-manifest.e4d4bb2eeded54c23eddf303c3e514e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "741f5c4f672d40cead84",
+    "url": "/clicky-game/static/js/main.741f5c4f.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/clicky-game/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "741f5c4f672d40cead84",
+    "url": "/clicky-game/static/css/main.70264a97.chunk.css"
+  },
+  {
+    "revision": "63e429690e65ed4840518dbedba9e34e",
+    "url": "/clicky-game/index.html"
+  }
+];
